055f11deafc558854c80edba1cf2351b5a24d545
Joe Xia <joexiayong@foxmail.com>
2017-10-18 21:14:51 +0800

    signle page finished

--- Eshow_details.py
+++ Eshow_details.py
@@ -8,6 +8,7 @@ r.encoding = r.apparent_encoding
 
 
 soup = BeautifulSoup(r.text,'html.parser')
+title=soup.find('h1').string.strip()
 l= soup('div',class_='zhxxcontent')
 a=l[0].contents
 date = a[3].string[5:]
@@ -19,5 +20,18 @@ area = a[15].string.strip()[5:]
 times = a[17].string[5:]
 cycle = a[19].string[5:]
 
-print(date,hall,industry,city,organizer,area,times,cycle)
+zhgkcon= soup('div',class_='zhgkcon')
+s=''
+intr= zhgkcon[0].contents
+for child in intr[1]:
+    s= s+child.string
+introduce = s.replace('\n','').replace('\xa0','')
+
+ran=''
+for child in zhgkcon[1]:
+    ran=ran+child.string
+ran = ran.replace('\n','').replace('\xa0','')
+
+
+print(date,hall,industry,city,organizer,area,times,cycle,introduce,ran)
 

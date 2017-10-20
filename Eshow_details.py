66f3439e89bb515af740725734905fa226d5d196
Joe Xia <joexiayong@foxmail.com>
2017-10-20 20:36:39 +0800

    hard to do

--- Eshow_details.py
+++ Eshow_details.py
@@ -16,37 +16,45 @@ for result in cursor1:
     r.encoding = r.apparent_encoding
 
     soup = BeautifulSoup(r.text,'html.parser')
-    title=soup.find('h1').string.strip()
+    try:
+        title=soup.find('h1').string.strip()
+    except:
+        title=''
+        continue
     l= soup('div',class_='zhxxcontent')
-    a=l[0].contents
-    dateTime = a[3].string.strip()[5:]
-    if a[5].string :
-        hall = a[5].string.strip()[5:].replace('\xa0','')
+    pTag = l[0].find_all('p')
+    dateTime = pTag[2].string.strip()[5:]
+    if pTag[3].string :
+        hall = pTag[3].string.strip()[5:].replace('\xa0','')
     else:
-        b=a[5].contents
-        hall = b[1].string.replace('\xa0','')
-    industry = a[7].find('a').string
-    city = a[9].string.strip()[5:]
-    organizer = a[11].string.strip()[5:]
-    area = a[13].string.strip()[5:]
-    times = a[15].string[5:]
-    cycle = a[17].string[5:]
+        hall = pTag[3].find('a').string
+    industry = pTag[4].find('a').string
+    city = pTag[5].string.strip()[5:]
+#    times = [15].string[5:]
+#    cycle = a[17].string[5:]
 
     zhgkcon= soup('div',class_='zhgkcon')
     s=''
     intr= zhgkcon[0].contents
     for child in intr[1]:
-        s= s+child.string
+        try:
+            s= s+child.string
+        except:
+            continue
+
     introduce = s.replace('\n','').replace('\xa0','')
 
     ran=''
     for child in zhgkcon[1]:
-        ran=ran+child.string
-    ran = ran.replace('\n','').replace('\xa0','')
+        try:
+            ran=ran+child.string
+        except:
+            continue   
+        ran = ran.replace('\n','').replace('\xa0','')
     cursor2.execute("insert into details(dateTime,hall,industry,city,\
-        organizer,area,times,cycle,introduce,ran) values\
-        ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');" %(dateTime,hall,industry,\
-        city,organizer,area,times,cycle,introduce,ran)
+        introduce,ran) values\
+        ('%s','%s','%s','%s','%s','%s');" %(dateTime,hall,industry,\
+        city,introduce,ran)
 )
     db2.commit()
 db1.close()
